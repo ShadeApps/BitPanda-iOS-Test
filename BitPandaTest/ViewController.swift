cb01cfb347b48dcaf77e1dad6ad613ef10f3388b
Sergey Grischyov <sergey@shadeapps.com>
2021-02-28 17:37:13 +0300

    Initial Project State

---
+++ BitPandaTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BitPandaTest
+//
+//  Created by Sergey Grischyov on 28.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
